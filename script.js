d660e70cb7c63836d008eac9811da12fa336113e
Hridoy Munda <hridoymnd10@gmail.com>
2026-03-08 00:04:54 +0600

    added login logic

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+
+const signInButton = document.getElementById("signin_btn");
+const mainBody = document.getElementById("main_body");
+signInButton.addEventListener("click", () => {
+        const userName = document.getElementById("username");
+        const password = document.getElementById("password");
+        const formContainer = document.getElementById("form_container");
+        const mainContainer = document.getElementById("main_container");
+
+
+        // // condition check 
+        if (userName.value === "admin" && password.value === "admin123") {
+            formContainer.classList.add("hidden")
+            mainContainer.classList.remove("hidden")
+
+        } else {
+            alert("Something went wrong")
+        }
+
+})
